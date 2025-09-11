daed559968ee768359528dac69c6024a0822df59
fakhri <arizafakhri@gmail.com>
2025-09-11 21:12:05 +0700

    wip

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+import Feedback from "../Feedback/Feedback";
 import CarouselHome from "./CarouselHome";
 import HeaderHome from "./HeaderHome";
 import LogoCompany from "./LogoCompany";
@@ -5,8 +6,10 @@ import Mitra from "./Mitra";
 import NewsItem from "./NewsItem";
 import Publikasi from "./Publikasi";
 import Video from "./Video";
+import React, { useState } from "react";
 
 function HomePage() {
+  const [isModalOpen, setIsModalOpen] = useState(true);
   return (
     <>
       <HeaderHome />
@@ -16,6 +19,10 @@ function HomePage() {
       <Publikasi />
       <Video />
       <Mitra />
+      <Feedback
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />{" "}
     </>
   );
 }
