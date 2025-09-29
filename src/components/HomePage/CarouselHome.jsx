85b333b0d5cff9d4ecdc9122a59d39b2deb66910
fakhri <arizafakhri@gmail.com>
2025-09-29 14:17:24 +0700

    wip

--- src/components/HomePage/CarouselHome.jsx
+++ src/components/HomePage/CarouselHome.jsx
@@ -1,9 +1,10 @@
+import { useRef } from "react";
 import styles from "./homepage.module.css";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
+import { Pagination } from "swiper/modules";
 import "swiper/css";
-import "swiper/css/navigation";
 import "swiper/css/pagination";
+
 import foto1 from "../../assets/foto1.png";
 import foto2 from "../../assets/foto3new.png";
 import foto4 from "../../assets/amelpic.jpg";
@@ -12,6 +13,40 @@ import foto5 from "../../assets/fotoasset1.jpg";
 const photos = [foto1, foto4, foto2, foto5];
 
 export default function CarouselHome() {
+  const swiperRef = useRef(null);
+  const intervalRef = useRef(null);
+
+  const startAutoScroll = (direction) => {
+    stopAutoScroll();
+
+    if (swiperRef.current) {
+      // langsung gerak sekali pas hover
+      if (direction === "next") {
+        swiperRef.current.slideNext();
+      } else {
+        swiperRef.current.slidePrev();
+      }
+    }
+
+    // lanjut auto scroll tiap 2 detik
+    intervalRef.current = setInterval(() => {
+      if (swiperRef.current) {
+        if (direction === "next") {
+          swiperRef.current.slideNext();
+        } else {
+          swiperRef.current.slidePrev();
+        }
+      }
+    }, 300);
+  };
+
+  const stopAutoScroll = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
+  };
+
   return (
     <div className={styles.containerCarousel}>
       {/* Left content */}
@@ -38,7 +73,27 @@ export default function CarouselHome() {
 
       {/* Right carousel */}
       <div className={styles.right}>
-        <Swiper spaceBetween={20} slidesPerView={2.5} loop={true}>
+        {/* Left hover zone */}
+        <div
+          className={styles.hoverZoneLeft}
+          onMouseEnter={() => startAutoScroll("prev")}
+          onMouseLeave={stopAutoScroll}
+        />
+
+        {/* Right hover zone */}
+        <div
+          className={styles.hoverZoneRight}
+          onMouseEnter={() => startAutoScroll("next")}
+          onMouseLeave={stopAutoScroll}
+        />
+
+        <Swiper
+          spaceBetween={20}
+          slidesPerView={2.5}
+          loop={true}
+          modules={[Pagination]}
+          onSwiper={(swiper) => (swiperRef.current = swiper)}
+        >
           {photos.map((src, index) => (
             <SwiperSlide key={index}>
               <img
